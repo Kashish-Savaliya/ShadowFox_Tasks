885424fba620b55651fedf6b8342d5cef92764fa
Kashish <[kashishsavaliya10@gmail.com]>
2024-08-24 14:59:26 +0530

    Added Beginner Task2

---
+++ Beginner/Task2.py
@@ -0,0 +1,23 @@
+# Program 1
+# This function will convert the decimal number to octal format
+# def format_number(number, fmt):
+#     formatted_string = format(number, fmt)
+#     return formatted_string
+#
+#
+# if __name__ == "__main__":
+    # print(format_number(145, 'o'))
+
+# Program 2
+# r = 84
+# area = (22/7) * r * r
+# total_water = int(1.4 * area)
+# print(f"The area of pond is: {area}")
+# print(f"The total amt of water in the pond is: {total_water}")
+
+# Program 3
+mins = 7
+time = mins * 60
+dist = 490
+speed = int(dist/time)
+print(f"The speed in m/s is: {speed}")
