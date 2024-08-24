ec2c66c0ab120d6ecbb6b9e0b13b0580da209945
Kashish <[kashishsavaliya10@gmail.com]>
2024-08-25 01:06:13 +0530

    Added Beginner Task7

---
+++ Task7.py
@@ -0,0 +1,39 @@
+# Program 1
+file_path = 'C:\\Users\\abc\\OneDrive\\Desktop\\ShadowFox\\task7.csv'
+
+with open(file_path, mode='r') as file:
+    content = file.read()
+
+lines = content.splitlines()
+
+header = lines[0].split(',')   #First Line
+list_of_dicts = []
+for line in lines[1:]:
+    row = line.split(',')
+    dict = {}
+    for i in range(len(header)):
+        dict[header[i]] = row[i]
+
+    list_of_dicts.append(dict)
+
+for ele in list_of_dicts:
+    total_marks = int(ele['Science_marks']) + int(ele['Maths_marks']) + int(ele['English_marks'])
+    average_marks = total_marks/3
+    ele['total_marks'] = total_marks
+    ele['average_marks'] = int(average_marks)
+
+for ele in list_of_dicts:
+    print(ele)
+
+
+# Program 2
+file2_path = 'C:\\Users\\abc\\OneDrive\\Desktop\\ShadowFox\\task7_modified.csv'
+header.append('total_marks')
+header.append('average_marks')
+
+with open(file2_path, mode='w') as file:
+    file.write(','.join(header)+'\n')
+
+    for ele in list_of_dicts:
+        row = [str(ele[col]) for col in header]
+        file.write(','.join(row)+'\n')
