4d72c175b5fda2aeee5e72326cca47e73dfd0253
Kashish <[kashishsavaliya10@gmail.com]>
2024-08-24 12:35:47 +0530

    Added Beginner Task1

---
+++ Beginner/Task1.py
@@ -0,0 +1,14 @@
+# Program 1
+# pi = 22/7
+# print(type(pi))
+
+# Program 2
+# for = 4     # Error : Because 'for' is a reserved keyword and caan't be used as a variable name
+
+# Program 3
+P = int(input('Enter principal amount : '))
+R = int(input('Enter the rate of interest : '))
+T = 3
+simple_interest = (P * R * T) / 100
+
+print(simple_interest)
